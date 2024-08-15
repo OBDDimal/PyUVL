865e2936091b106bdf10d2cf881fbfc062d7d9f0
chris <c.r.vogel@arcor.de>
2024-08-15 14:11:09 +0200

    add: Button to save the file

--- index.js
+++ index.js
@@ -5,63 +5,64 @@ import { EditorView, basicSetup } from 'codemirror';
 import { javascript } from '@codemirror/lang-javascript';
 
 class CodeEditor extends LitElement {
-  static styles = css`
+    static styles = css`
     .editor-container {
       border: 1px solid #ddd;
       height: 400px;
     }
   `;
 
-  constructor() {
-    super();
-    this.editor = null;
-  }
-
-  firstUpdated() {
-    this.editor = new EditorView({
-      doc: 'console.log("Edit your UVL Code here!");',
-      extensions: [basicSetup, javascript()],
-      parent: this.renderRoot.querySelector('.editor-container')
-    });
-
-    const openButton = document.getElementById('openButton');
-    const saveButton = document.getElementById('saveButton');
-    const fileInput = document.getElementById('fileInput');
-
-    openButton.addEventListener('click', () => fileInput.click());
-    fileInput.addEventListener('change', this.handleFileOpen.bind(this));
-    saveButton.addEventListener('click', this.handleFileSave.bind(this));
-  }
-
-  handleFileOpen(event) {
-    const file = event.target.files[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = (e) => {
-        this.editor.dispatch({
-          changes: { from: 0, to: this.editor.state.doc.length, insert: e.target.result }
-        });
-      };
-      reader.readAsText(file);
+    constructor() {
+        super();
+        this.editor = null;
     }
-  }
 
-  handleFileSave() {
-    //does not work yet. Probably a playground problem.
-    const blob = new Blob([this.editor.state.doc.toString()], { type: 'text/plain' });
-    const url = URL.createObjectURL(blob);
-    const a = document.createElement('a');
-    a.href = url;
-    a.download = 'code.txt';
-    a.click();
-    URL.revokeObjectURL(url);
-  }
+    firstUpdated(_changedProperties ) {
+        this.editor = new EditorView({
+            doc: 'console.log("Edit your UVL Code here!");',
+            extensions: [basicSetup, javascript()],
+            parent: this.renderRoot.querySelector('.editor-container')
+            //parent: document.body
+        });
 
-  render() {
-    return html`
+        const openButton = document.getElementById('openButton');
+        const saveButton = document.getElementById('saveButton');
+        const fileInput = document.getElementById('fileInput');
+
+        openButton.addEventListener('click', () => fileInput.click());
+        fileInput.addEventListener('change', this.handleFileOpen.bind(this));
+        saveButton.addEventListener('click', this.handleFileSave.bind(this));
+    }
+
+    handleFileOpen(event) {
+        const file = event.target.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = (e) => {
+                this.editor.dispatch({
+                    changes: { from: 0, to: this.editor.state.doc.length, insert: e.target.result }
+                });
+            };
+            reader.readAsText(file);
+        }
+    }
+
+    handleFileSave() {
+        //does not work yet. Probably a playground problem.
+        const blob = new Blob([this.editor.state.doc.toString()], { type: 'text/plain' });
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = 'code.txt';
+        a.click();
+        URL.revokeObjectURL(url);
+    }
+
+    render() {
+        return html`
       <div class="editor-container"></div>
     `;
-  }
+    }
 }
 
 customElements.define('code-editor', CodeEditor);
