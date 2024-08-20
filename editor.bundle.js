ea789453990b88a4d4784267602023395a33400b
chris <c.r.vogel@arcor.de>
2024-08-20 07:44:49 +0200

    fix: file saving didnt save current state

--- editor.bundle.js
+++ editor.bundle.js
@@ -28753,7 +28753,7 @@
        parent: document.querySelector("#editor"),
    });
 
-   //safe
+   //save
    function saveToFile(content, filename) {
        const blob = new Blob([content], { type: 'text/plain' });
        const url = URL.createObjectURL(blob);
@@ -28766,15 +28766,6 @@
        URL.revokeObjectURL(url);
    }
 
-   // Event-Listener for safe
-   document.querySelector("#save-button").addEventListener("click", () => {
-       const content = myView.state.doc.toString();
-       const filename = "editor-content.txt";
-       saveToFile(content, filename);
-   });
-   //Listener for open
-   document.querySelector("#file-input").addEventListener("change", loadFile);
-
    //load file
    function loadFile(event) {
        const file = event.target.files[0];
@@ -28784,11 +28775,24 @@
        const reader = new FileReader();
        reader.onload = function(e) {
            const content = e.target.result;
+           // update content
+           console.log(content);
            myView.dispatch({
                changes: { from: 0, to: myView.state.doc.length, insert: content }
            });
        };
        reader.readAsText(file);
    }
+
+   // Event-Listener for save
+   document.querySelector("#save-button").addEventListener("click", () => {
+       const content = myView.state.doc.toString();
+       const filename = "uvl_model.txt";
+       //ToDO: remove after debugging
+       console.log(content);
+       saveToFile(content, filename);
+   });
+   //Listener for open
+   document.querySelector("#file-input").addEventListener("change", loadFile);
 
 })();
--- editor.js
+++ editor.js
@@ -20,7 +20,7 @@ const myView = new EditorView({
     parent: document.querySelector("#editor"),
 })
 
-//safe
+//save
 function saveToFile(content, filename) {
     const blob = new Blob([content], { type: 'text/plain' });
     const url = URL.createObjectURL(blob);
@@ -33,15 +33,6 @@ function saveToFile(content, filename) {
     URL.revokeObjectURL(url);
 }
 
-// Event-Listener for safe
-document.querySelector("#save-button").addEventListener("click", () => {
-    const content = myView.state.doc.toString();
-    const filename = "editor-content.txt";
-    saveToFile(content, filename);
-});
-//Listener for open
-document.querySelector("#file-input").addEventListener("change", loadFile);
-
 //load file
 function loadFile(event) {
     const file = event.target.files[0];
@@ -51,9 +42,22 @@ function loadFile(event) {
     const reader = new FileReader();
     reader.onload = function(e) {
         const content = e.target.result;
+        // update content
+        console.log(content);
         myView.dispatch({
             changes: { from: 0, to: myView.state.doc.length, insert: content }
         });
     };
     reader.readAsText(file);
 }
+
+// Event-Listener for save
+document.querySelector("#save-button").addEventListener("click", () => {
+    const content = myView.state.doc.toString();
+    const filename = "uvl_model.txt";
+    //ToDO: remove after debugging
+    console.log(content);
+    saveToFile(content, filename);
+});
+//Listener for open
+document.querySelector("#file-input").addEventListener("change", loadFile);
