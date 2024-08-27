3759694f5ece60ef511fda39258d4edbb8f470d2
chris <c.r.vogel@arcor.de>
2024-08-28 01:34:15 +0200

    add: adding parser, example and grammar feat: deleting all the files which are not needed

--- highlight.js
+++
@@ -1,9 +0,0 @@
-import { HighlightStyle, tags } from "@codemirror/highlight";
-
-export const uvlHighlightStyle = HighlightStyle.define([
-    { tag: tags.keyword, color: '#007acc' },
-    { tag: tags.string, color: '#a31515' },
-    { tag: tags.variableName, color: '#267f99' },
-    { tag: tags.operator, color: '#d4d4d4' },
-    { tag: tags.brace, color: '#569cd6' }
-]);
