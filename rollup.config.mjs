6fa76d0aa89b957fec7a2a189818aacca325862c
Tobias Heß <me@h3ssto.net>
2024-09-09 11:03:43 +0200

    Changed deployment path

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,7 +2,7 @@ import {nodeResolve} from "@rollup/plugin-node-resolve"
 export default {
     input: "src/editor.mjs",
     output: {
-        file: "src/editor.bundle.js",
+        file: "dist/js/editor.bundle.js",
         format: "iife"
     },
     plugins: [nodeResolve()]
